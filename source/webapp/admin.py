b87e04531065bb03607dc46f92d8845a0fb32838
Ray888Ray <ilimirgashev@gmail.com>
2022-12-10 18:51:07 +0600

    admin panel set up

---
+++ source/webapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from webapp.models import Pull, Choice, Answer
+# Register your models here.
+
+admin.site.register(Pull)
+admin.site.register(Choice)
+admin.site.register(Answer)
